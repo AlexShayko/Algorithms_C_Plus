0ae97464917c0772cfbb366cabf8a1987dbe5e3c
AlexShayko <alex.shayko04@gmail.com>
2026-06-23 14:46:19 +0300

    Create solution_B.cpp

---
+++ hw2/solutions/solution_B.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <queue>
+
+int main() {
+    int m = 0;
+    std::cin >> m;
+    std::vector<std::vector<int>> gr;
+    int counter = 0;
+    std::map<std::string, int> tr;
+    for (int i = 0; i < m; ++i) {
+        std::string from, arrow, to;
+        std::cin >> from >> arrow >> to;
+        if (!tr.contains(from)) {
+            tr[from] = counter;
+            ++counter;
+            gr.emplace_back();
+        }
+        if (!tr.contains(to)) {
+            tr[to] = counter;
+            ++counter;
+            gr.emplace_back();
+        }
+        gr[tr[from]].push_back(tr[to]);
+    }
+    std::string start, end;
+    std::cin >> start >> end;
+
+    if (start == end) {
+        std::cout << 0;
+        return 0;
+    }
+
+    if (!tr.contains(start) || !tr.contains(end)) {
+        std::cout << -1;
+        return 0;
+    }
+    int st = tr[start];
+    int en = tr[end];
+
+    std::queue<int> q;
+    std::vector<int> dist(tr.size(), -1);
+    dist[st] = 0;
+    q.push(st);
+    while (!q.empty()) {
+        int v = q.front();
+        q.pop();
+        for (int to : gr[v]) {
+            if (dist[to] == -1) {
+                dist[to] = dist[v] + 1;
+                q.push(to);
+            }
+        }
+    }
+
+    std::cout << ((dist[en] == -1) ? -1 : dist[en]);
+
+    return 0;
+}
